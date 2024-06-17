9159741cd37e812bdf1b74e1a8953ec7fe21392c
Anjin Lee <anjin720jin@naver.com>
2024-06-17 22:34:41 +0900

    first init

---
+++ PDFEraser/PDFEraserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFEraserApp.swift
+//  PDFEraser
+//
+//  Created by Anjin on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PDFEraserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
