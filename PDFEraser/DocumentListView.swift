a58065409d8e8000233e6091b5b5a8f00572b886
Seon-U <seonu.kim.kr@gmail.com>
2024-06-18 20:43:52 +0900

    feat/DocumentListView 생성 ( 내부 썸네일 불러오는 기능과 URL에서 파일명 불러오는 기능 추가 )

---
+++ PDFEraser/DocumentListView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentListView.swift
+//  PDFEraser
+//
+//  Created by seonu kim on 6/18/24.
+//
+
+import SwiftUI
+
+struct DocumentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentListView()
+}
