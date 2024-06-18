83ad0cd2e586c166878246fa026c7dc474d7e9f1
Anjin Lee <anjin720jin@naver.com>
2024-06-18 17:48:24 +0900

    Feat: 공유 시트 추가

--- PDFEraser/EraserView.swift
+++ PDFEraser/EraserView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct EraserView: View {
     let url: URL
@@ -45,8 +46,36 @@ struct EraserView: View {
             }
         }
         .navigationTitle("\(url.lastPathComponent)")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    saveAndSharePDF()
+                } label: {
+                    Image(systemName: "square.and.arrow.up")
+                        .tint(Color.ioowRed)
+                }
+            }
+        }
         .onAppear {
             pdfKitView = PDFKitView(url: url)
+        }
+    }
+    
+    func saveAndSharePDF() {
+        guard let pdfDocument = pdfKitView?.pdfView.document else { return }
+        
+        // PDF 파일 저장 경로 설정
+        let fileManager = FileManager.default
+        guard let outputURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("AnnotatedPDF.pdf") else { return }
+        
+        // PDF 문서 저장
+        pdfDocument.write(to: outputURL)
+        
+        // 공유 시트 표시
+        let activityViewController = UIActivityViewController(activityItems: [outputURL], applicationActivities: nil)
+        
+        if let topController = UIApplication.shared.windows.first?.rootViewController {
+            topController.present(activityViewController, animated: true, completion: nil)
         }
     }
 }
