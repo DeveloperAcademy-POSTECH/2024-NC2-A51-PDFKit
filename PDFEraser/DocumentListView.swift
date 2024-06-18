09c10e78217f138df04e6b96cbc75ff4b4009426
Seon-U <seonu.kim.kr@gmail.com>
2024-06-18 21:12:40 +0900

    주석 및 썸네일을 불러올 수 없을 경우 실행할 이미지 교체

--- PDFEraser/DocumentListView.swift
+++ PDFEraser/DocumentListView.swift
@@ -12,6 +12,8 @@ struct DocumentListView: View {
     @State private var image: UIImage?
     //url 경로 변수
     //지금은 번들로 예시 작성
+    //앞으로 불러온다면 저장된 파일의 url 배열을 통해 반복 호출하면 될 듯함!
+    
     var url = Bundle.main.url(forResource: "sample", withExtension: "pdf")
     
     var body: some View {
@@ -19,10 +21,16 @@ struct DocumentListView: View {
             if let image = image {
                 Image(uiImage: image)
                     .frame(width: 100, height: 100)
+                
             } else {
-                Text("Failed to generate thumbnail")
+                //fail 시 기본 doc 이미지 보여주기
+                Image(systemName: "doc.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.ioowRed)
             }
-            
+            //URL 언랩
             if let unwrapedURL = url {
                 Text("\(URLtoFileName(url: unwrapedURL))")
                     .lineLimit(2)
@@ -35,15 +43,21 @@ struct DocumentListView: View {
             }
         }
     }
+    //썸네일 만드는 func
     func generateThumbnail(url : URL) -> UIImage? {
         guard let document = PDFDocument(url: url) else { return nil }
         guard let page = document.page(at: 0) else { return nil }
+        //사이즈 조절
         let image = page.thumbnail(of: CGSize(width: 100, height: 100), for: .cropBox)
         return image
     }
     
+    //url을 통해 file 명을 불러오는 func
     func URLtoFileName(url : URL) -> String {
+        //url을 문자열로 변환
         let urlString = "\(url)"
+        //정규표현식
+        // / 뒤에부터 .뒤에 3~4번 반복되는 문자열까지 제일 끝에서부터 읽음
         let pattern = "[^/]+\\.[a-zA-Z]{3,4}$"
 
         if let regex = try? NSRegularExpression(pattern: pattern),
@@ -51,7 +65,7 @@ struct DocumentListView: View {
             let fileNameRange = Range(match.range, in: urlString)!
             let fileName = String(urlString[fileNameRange])
 
-            return fileName // Output: document.pdf
+            return fileName // Output: sample.pdf
         } else {
             return "파일명을 불러오지 못했습니다."
         }
