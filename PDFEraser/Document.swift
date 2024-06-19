6001a2ba1d988a729bdef08e0c8113ce80bc0ac4
Anjin Lee <anjin720jin@naver.com>
2024-06-19 17:01:08 +0900

    Fix: 썸네일 크기 조정

--- PDFEraser/Document.swift
+++ PDFEraser/Document.swift
@@ -17,7 +17,7 @@ class Document {
     
     @Transient var thumbnailImage: Image {
         guard let page = PDFDocument(url: url)?.page(at: 0) else { return Image("") }
-        return Image(uiImage: page.thumbnail(of: CGSize(width: 100, height: 100), for: .cropBox))
+        return Image(uiImage: page.thumbnail(of: CGSize(width: 300, height: 300), for: .cropBox))
     }
     
     init(url: URL) {
